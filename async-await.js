95d86b196e06a4d019f4b73ef51a254605c9f20a
nwre87 <nwre87@gmail.com>
2017-05-29 17:23:47 -1000

    add babel-cli - async-await

--- async-await.js
+++ async-await.js
@@ -1,1 +1,38 @@
 const fetch = require('node-fetch');
+const username = process.argv[2];
+
+const {
+  fetchUser,
+  fetchRepos,
+  fetchGists,
+} = require('./gh-requests');
+
+async function getCombined() {
+  const userRes =  await fetchUser(username);
+  const user = await userRes.json();
+  if (user.message === 'Not Found') throw new Error(user.message); 
+
+  const reposRes = await fetchRepos(username);
+  const repos = await reposRes.json();
+
+  const gistsRes = await fetchGists(username);
+  const gists = await gistsRes.json();
+
+  return [
+    user,
+    repos,
+    gists
+  ];
+};
+
+(async () => {
+  try {
+    const combined = await getCombined();
+    console.log(combined)
+  }
+  catch(error) {
+    console.log(error);
+  }
+  
+})();
+
